9fb2686de1df5e74b901ee3d99ee534e63798909
paulo <pauloccc010@gmail.com>
2025-11-28 13:32:49 -0300

    troca de nome arquivo py

--- Backend/app.py
+++ Backend/app.py
@@ -3,6 +3,9 @@ from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 
+app = Flask(__name__)
+CORS(app)
+
 # Fazendo a requisição para a página de notícias
 url = 'https://www.globo.com/'
 response = requests.get(url)
